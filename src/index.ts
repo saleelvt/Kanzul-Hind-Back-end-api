ecbfcc4b92d4c23cdbff7ac31dfc369a9e865a0c
saleel vt <saleelvt57@gmail.com>
2025-03-21 11:24:32 +0530

    initial

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+// main file (e.g., index.ts)
+
+
+import { db } from "./boot/db";
+import dotenv from "dotenv";
+import { Admin } from "./infrastructure/database/models/adminSchema";
+import app from "@/presentation/server";
+dotenv.config(); // Load environment variables
+
+// Function to initialize an admin (example)
+export const insertAdmin = async () => {
+  const sampleAdmin = {
+    userName: "nourAlSafwa",
+    email: "admin12@gmail.com",
+    password: "admin@12",
+  };
+  // Insert logic for saving admin to the database if required here
+
+  try {
+    const existingAdmin = await Admin.findOne({ email: sampleAdmin.email });
+    if (!existingAdmin) {
+      const newAdmin = new Admin(sampleAdmin);
+      await newAdmin.save();
+      console.log("this is the admin now ", newAdmin);
+    } else {
+      console.log(" ADMIN ALLREDY EXISTED  ");
+    }
+  }  catch (error) {
+    console.error("Failed to insert sample admin:", error);
+  }
+};
+
+(async () => {
+  try {
+    console.log("Initializing server and database connection...");
+    // Start database connection
+    await db()
+      .then(() => console.log("Database connected in index page finish"))
+      .catch((error: any) => {
+        console.error("Error while connecting MongoDB", error);
+        process.exit(0); // Exit on DB connection failure
+      });
+    // Start the server
+    app.listen(Number(process.env.PORT) || 1001, () => {
+      console.log(`Server started on port ${process.env.PORT || 1001}`);
+    });
+
+  } catch (error: any) {
+    console.log("Error on start up: ", error);
+  } finally {
+    process.on("SIGINT", async () => {
+      console.log("Server is shutting down...");
+      process.exit();
+    });
+  }
+})();
+
+// insertAdmin()
